176370b392665a65141911351fa2a21f7b23a85c
Kent Yip <23076078@student.uwa.edu.au>
2024-05-20 21:55:50 +0800

    Fixed to match Ryan's changes.

--- app/main/routes.py
+++ app/main/routes.py
@@ -43,15 +43,15 @@ def get_file(filename):  # pragma: no cover
         return str(exc)
     
 
-@bp.route('/classroom/<path:path>', methods=['GET'])
+@bp.route('/public/<path:path>', methods=['GET'])
 def get_resource(path):  # pragma: no cover
     mimetypes = {
         ".css": "text/css",
         ".html": "text/html",
         ".js": "application/javascript",
     }
-    complete_path = os.path.join(root_dir(), "../../public/" + path)
-    ext = os.path.splitext("../../public/" + path)[1]
+    complete_path = os.path.join(root_dir(), "../public/" + path)
+    ext = os.path.splitext("../public/" + path)[1]
     mimetype = mimetypes.get(ext, "text/html")
     content = get_file(complete_path)
     return Response(content, mimetype=mimetype)
